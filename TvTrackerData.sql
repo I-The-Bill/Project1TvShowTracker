86e61c8db51cf57eb0992c4f7ed0a4b4135d10b9
Jalamang <Jalamangjanka@gmail.com>
2022-10-13 13:58:21 -0400

    Added seeddata for Category_show table

--- TvTrackerData.sql
+++ TvTrackerData.sql
@@ -54,5 +54,19 @@ VALUES
 	(null,'Drama'),
     (null,'anime');
 
-    -- SELECT FROM TV_show
+    -- SELECT FROM Category
 SELECT * FROM Category;
+
+
+-- INSERT INTO TABLE category_show
+INSERT INTO 
+	Category(show_id, category_id)
+VALUES
+    (1,1), (2,2), (3,3),(4,4), (5,5),
+    (6,6), (7,7), (8,8), (9,9), (10,10),
+    (11,11), (12,12), (13,13), (14,14),
+    (15,15), (16,16), (17,17), (18,18),
+    (19,19), (20,20);
+
+        -- SELECT FROM category_show
+SELECT * FROM category_show;
