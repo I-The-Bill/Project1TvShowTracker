5462e2a99691874e03e02d74edf5419830df454b
Jalamang <Jalamangjanka@gmail.com>
2022-10-13 13:54:26 -0400

    Added seeddata for TV_show

--- TvTrackerData.sql
+++ TvTrackerData.sql
@@ -1,1 +1,30 @@
-use TvTrackerSchema;
+use TvTracker;
+
+-- INSERT INTO TABLE TV_show
+INSERT INTO 
+	TV_show(show_id, show_name, episode_count)
+VALUES
+	(null, 'Bleach', '366'),
+    (null, 'Soul Eater', '8'),
+    (null, 'Star Wars Andor', '8'),
+    (null, 'Star Wars The Clone Wars', '8'),
+    (null, '60 Days In', '5'),
+    (null, 'Dahmer', '8'),
+    (null, 'Narcos', '30'),
+    (null, 'Breaking Bad', '62'),
+    (null, 'House of the Dragon ', '17'),
+    (null, 'The Lord of the Rings: The Rings of Power', '3'),
+    (null, 'She-Hulk: Attorney at Law', '78'),
+    (null, "The Handmaid's Tale", '0'),
+    (null, "Stranger Things", '0'),
+    (null, "House of the Dragon", '0'),
+    (null, "Game of Thrones", '73'),
+    (null, "Rick and Morty", '3'),
+    (null, "Friday Night Light", '76'),
+    (null, "The Punisher", '26'),
+	(null, "One Piec", '10'),
+	(null, "Grey’s anatomy ", '0');
+
+-- SELECT FROM TV_show
+SELECT * FROM TV_show;
+
